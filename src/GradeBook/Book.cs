bdca95439a3cecb9fd8bf86c9a489644c7692009
abowler <abowler2@gmail.com>
2019-07-19 13:14:57 -0700

    added abstracts and refactored

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,18 +1,82 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace GradeBook
 {
 
     public delegate void GradeAddedDelegate(object sender, EventArgs args);
-    public class Book 
+
+    public class NamedObject
     {
-        public Book(string name)
+        public NamedObject(string name)
+        {
+            Name = name;
+        }
+    
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    public interface IBook
+        {
+            void AddGrade(double grade);
+            Stats GetStats();
+            string Name { get; }
+            event GradeAddedDelegate GradeAdded;
+        }
+
+    public abstract class Book : NamedObject, IBook
+    {
+        public Book(string name) : base(name)
+        {
+        }
+
+        public abstract event GradeAddedDelegate GradeAdded;
+
+        public abstract void AddGrade(double grade);
+
+        public abstract Stats GetStats();
+    }
+
+    public class DiskBook : Book
+    {
+        public DiskBook(string name) :base(name)
+        {
+            
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+
+        public override void AddGrade(double grade)
+        {
+            using(var writer = File.AppendText($"{Name}.txt"))
+            {
+                writer.WriteLine(grade);
+                if(GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+        }
+
+        public override Stats GetStats()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InMemoryBook : Book
+    {
+        public InMemoryBook(string name) : base(name)
         {
             grades = new List<double>();
             Name = name;
         }
-        public void AddGrade(double grade)
+        public override void AddGrade(double grade)
         {
             if(grade <= 100 && grade >= 0)
             {
@@ -28,8 +92,8 @@ namespace GradeBook
             }
         }
 
-        public event GradeAddedDelegate GradeAdded;
-        public Stats GetStats()
+        public override event GradeAddedDelegate GradeAdded;
+        public override Stats GetStats()
         {
             var result = new Stats();
             result.Average = 0.0;
@@ -69,11 +133,5 @@ namespace GradeBook
         }
 
         private List<double> grades;
-        
-        public string Name
-        {
-            get; 
-            set;
-        }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -6,9 +6,21 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book("Grade Book");
+            IBook book = new DiskBook("April's Grade Book");
             book.GradeAdded += OnGradeAdded;
-            
+
+            EnterGrades(book);
+
+            var stats = book.GetStats();
+            Console.WriteLine($"For the book named {book.Name}:");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average letter grade is {stats.Letter}");
+        }
+
+        private static void EnterGrades(IBook book)
+        {
             while (true)
             {
                 Console.WriteLine("Please enter grade: (press 'q' when done)");
@@ -23,22 +35,15 @@ namespace GradeBook
                     var grade = double.Parse(input);
                     book.AddGrade(grade);
                 }
-                catch(ArgumentException ex)
+                catch (ArgumentException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-                catch(FormatException ex)
+                catch (FormatException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
-            } 
-            
-            var stats = book.GetStats();
-            Console.WriteLine($"For the book named {book.Name}:");
-            Console.WriteLine($"The average grade is {stats.Average:N1}");
-            Console.WriteLine($"The lowest grade is {stats.Low}");
-            Console.WriteLine($"The highest grade is {stats.High}");
-            Console.WriteLine($"The average letter grade is {stats.Letter}");
+            }
         }
 
         static void OnGradeAdded(object sender, EventArgs e)
